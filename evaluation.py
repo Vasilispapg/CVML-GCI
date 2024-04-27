a50fb992b74c0365aea3e512db0db0d135e327ce
Vasilis Papagrigoriou <vasilispapg@outlook.com>
2024-04-27 22:47:36 +0300

    evaluation got correct captions but wrong loss

--- evaluation.py
+++ evaluation.py
@@ -1,7 +1,25 @@
 import torch
 import torch.nn as nn
+import matplotlib.pyplot as plt
 
-def evaluate_model(device, model, data_loader):
+def generate_caption(output, vocab):
+    """ Generate a caption from the model output """
+    caption = []
+    output = output.permute(1,0)
+    # output [vocab_size, 1]
+    probs = torch.softmax(output, dim=0)
+    word_idx = torch.argmax(probs, dim=1)
+    for idx in word_idx:
+        word = idx.item()
+        if word == vocab.stoi['<end>']:
+            break
+        if(word == vocab.stoi['<start>'] or word == vocab.stoi['<pad>']):
+            continue
+        caption.append(vocab.itos[word])
+    return ' '.join(caption)
+   
+
+def evaluate_model(device, model, data_loader,vocab):
     """ Evaluate the model on a given dataset """
     model.eval()  # Set the model to evaluation mode
     model = model.to(device)
@@ -13,19 +31,29 @@ def evaluate_model(device, model, data_loader):
             captions = captions.to(device)
             
             # Forward pass: compute predicted outputs by passing inputs to the model
-            outputs = model(imgs, captions)
             breakpoint()
+            outputs , padding_mask = model(imgs, captions)
+            # outputs [batch_size, seq_len-1, vocab_size]
+            output = outputs.permute(1, 2, 0)
+            # [2941,42]
+
+            loss = criterion(output,captions)
+
+            loss_masked = torch.mul(loss, padding_mask)
+
+            final_batch_loss = torch.sum(loss_masked)/torch.sum(padding_mask)
             
-            # TODO Xtypaei edw
-            # Trim the last 2 timestep Not good practice
-            outputs = outputs[:, :-2, :]
+            # write the caption
+            outputs=outputs.permute(1,2,0)
+            print('Predicted:', generate_caption(outputs[0], vocab))
             
-            # Calculate the loss
-            loss = criterion(outputs.reshape(-1, outputs.size(2)), captions[:, 1:].reshape(-1))
+            # display image
+            plt.imshow(imgs[0].permute(1, 2, 0).cpu().numpy())
+            plt.show()
             
             # Output the loss for this batch
-            print(f'Batch {i + 1}, Loss: {loss.item()}')
+            print(f'Batch {i + 1}, Loss: {final_batch_loss.item()}')
             
-            # Stop after processing 6 batches
-            if i == 5:
+            # Stop after processing 5 batches
+            if i == 4:
                 break
