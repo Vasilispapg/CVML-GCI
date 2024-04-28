39f6ce4ebda27adc9b6a93dcc447b752ace15141
Vasilis Papagrigoriou <vasilispapg@outlook.com>
2024-04-28 15:50:44 +0300

    new load model

--- saveload.py
+++ saveload.py
@@ -7,10 +7,26 @@ def save_checkpoint(state, filename="model_checkpoint.pth.tar"):
     
 def load_checkpoint(checkpoint, model, optimizer=None):
     print("=> Loading checkpoint")
-    model.load_state_dict(checkpoint["state_dict"])
-    if(optimizer is None):
-        return model
-    optimizer.load_state_dict(checkpoint["optimizer"])
 
-    return model, optimizer
+    # Handle case where model might be wrapped inside a tuple
+    if isinstance(model, tuple):
+        print("Model is a tuple, extracting the model instance.")
+        model = model[0]  # Assuming the model is the first element in the tuple
+
+    model_state = checkpoint['model']
+    current_model_state = model.state_dict()
+
+    # Filter out unnecessary keys
+    filtered_model_state = {
+        k: v for k, v in model_state.items()
+        if k in current_model_state and current_model_state[k].size() == v.size()
+    }
+
+    model.load_state_dict(filtered_model_state, strict=False)
+
+    if optimizer:
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        return model, optimizer
+
+    return model
 
